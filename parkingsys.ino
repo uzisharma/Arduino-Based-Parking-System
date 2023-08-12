be55f0c706d3203f971425f7d8fa597904fc9ce4
uzisharma <141988110+uzisharma@users.noreply.github.com>
2023-08-12 11:52:39 +0530

    parkingsys.ino

--- parkingsys.ino
+++ parkingsys.ino
@@ -1,106 +1,135 @@
-//ARDUINO (automatic car parking) 
-//4TH SEMESTER MINI PROJECT [RVS COLLEGE OF ENGINEERING AND TECHNOLOGY]
+//ARDUINO (!!!!!!!!!!!!!!!AUTOMATIC PRAKING SYSTEM!!!!!!!!!!!!!!!!) 
+//4TH SEMESTER MINI-PROJECT [RVS COLLEGE OF ENGINEERING AND TECHNOLOGY]
 //BY:- 1, UJJWAL SHARMA
 //     2, SUAJN ADHIKARY
 
 
-
+#include <String.h>
 #include <Wire.h>           
-#include <LiquidCrystal_I2C.h>
-#include <String.h>    
-LiquidCrystal_I2C lcd(0x27,16,2);   
- #include <Servo.h>   
- Servo myservo1;  
- const int IR1 = 4; // IR Sensor 1  
- const int IR2 = 7; // IR Sensor 2  
- const int buzzer = 2;
- const int ledred = 13;
- const int ledgreen = 11;
- int Slot = 5;      // Total number of parking Slots  
+#include <LiquidCrystal_I2C.h>    
+    LiquidCrystal_I2C lcd(0x27,16,2);  
+#include <Servo.h>   
+    Servo gate;
+ 
+ const int IR1 = 6;        // IR Sensor 1  
+ const int IR2 = 7;        // IR Sensor 2  
+ const int buzzer = 2;     // Buzzer
+ const int ledred = 13;    // RedLED
+ const int ledgreen = 11;  // GreedLED
+ int Slot = 5;             // Total number of parking Slots  
  int flag1 = 0;  
  int flag2 = 0; 
 
+//Function to display onto the LCD
+      void lcdDisplay(String topline, String bottomline, int delay_time){
+              lcd.setCursor (0,0);
+              lcd.print(topline);
+              lcd.setCursor (0,1);
+              lcd.print(bottomline);
+              delay (delay_time);
+              if(delay_time>0)
+                  lcd.clear();
+      }
 
-void displayname(String names, String roll_no){
-        lcd.setCursor (0,0);
-        lcd.print(names);
-        lcd.setCursor (0,1);
-        lcd.print(roll_no);
-        delay (2000);
-        lcd.clear();
-}
+//Function to Calculate the Number of SLOTS LEFT
+      void slotUpdate(int &Slot, int &flag1, int &flag2){
+        if(flag1==1){
+             Slot -= 1;
+        }else if(flag2 ==1){
+          Slot +=1;
+        }
+         lcdDisplay("   Welcome!!!", "Slot Left : ", 0);
+         lcd.print(Slot);
+         
+      }
+
   
  void setup()  
       {
+        Serial.begin(9600);
         lcd.init();      
         lcd.backlight();  
         pinMode(IR1, INPUT);  
         pinMode(IR2, INPUT);
         pinMode(ledred,OUTPUT);
         pinMode(ledgreen,OUTPUT);  
-        myservo1.attach(9);  
-        myservo1.write(100);  
+        gate.attach(9);  
+        gate.write(100);  
 
-        String names[4] = {"Ujjwal Sharma", "Sujan Adhikary"};
-        String roll_num[4] = {" ECE/462/21L", " ECE/461/21L"};
+        String names[2] = {"Ujjwal Sharma", "Sujan Adhikary"};
+        String roll_num[2] = {" ECE/462/21L", " ECE/461/21L"};
         
-        displayname("PARKING SYSTEM", "      By  "); 
-        for(int i=0;i<4;i++){
-         displayname(names[i],roll_num[i]);
-          }     
+        lcdDisplay(" PARKING SYSTEM", "      By  ", 2000); 
+        for(int i=0;i<2;i++){
+         lcdDisplay(names[i],roll_num[i], 2000);
+          }
+        slotUpdate(Slot, flag1, flag2);     
       } 
 
-
-       
+ 
  void loop()
         {   
-           if(digitalRead (IR1) == LOW && flag1==0)
-                {  
-                  if(Slot>0)
-                       {
-                        flag1=1;  
-                           if(flag2==0)
-                                {
-                                  myservo1.write(0); 
-                                  Slot = Slot-1;
-                                  //if(digitalRead (IR1) == LOW){tone(buzzer, 3000);} if(digitalRead (IR1) == HIGH){noTone(buzzer);}
-                     tone(buzzer, 4000);
-                     delay(500);
-                     noTone(buzzer);
-                                 }
-                        }else{  
-                            lcd.setCursor (0,0);  
-                            lcd.print("  SORRY :(  ");   
-                            lcd.setCursor (0,1);  
-                            lcd.print(" Parking Full "); 
-                            tone(buzzer, 1000);
-                            
-                            digitalWrite(ledred, HIGH);
-                            digitalWrite(ledgreen,LOW); 
-                            delay (1000); 
-                            lcd.clear();
-                            if(digitalRead (IR1) == HIGH)
-                                    {
-                                      noTone(buzzer);   
-                                    }
-                              }  
-                }  
- if(digitalRead (IR2) == LOW && flag2==0){flag2=1;  
- if(flag1==0){
-  myservo1.write(0);
-  digitalWrite(ledred,LOW);
-  Slot = Slot+1;
-  }  
- }  
- if(flag1==1 && flag2==1 && digitalRead (IR2) == HIGH){  
- delay (1000);  
- myservo1.write(100); 
- flag1=0, flag2=0;  
- }  
- lcd.setCursor (0,0);  
- lcd.print("  WELCOME!  ");  
- lcd.setCursor (0,1);  
- lcd.print("Slot Left: ");  
- lcd.print(Slot);
- digitalWrite(ledgreen,HIGH);  
+    if(Slot==0){
+      digitalWrite(ledgreen, LOW);
+      digitalWrite(ledred, HIGH);
+    }else{
+      digitalWrite(ledgreen, HIGH);
+      digitalWrite(ledred,LOW);
+    }
+
+// CASE 1: When the Parking is Full, and no SLOTS are available    
+        if(digitalRead(IR1)==LOW && Slot==0){
+          while(digitalRead(IR1)==LOW){
+            lcdDisplay("   SORRY :(  ", "  Parking Full ", 0);
+            tone(buzzer, 1000);
+            digitalWrite(ledred, HIGH);
+            delay(300);
+            digitalWrite(ledred, LOW);
+            delay(300);
+          }
+          digitalWrite(ledred, LOW);
+          noTone(buzzer);
+          slotUpdate(Slot, flag1, flag2); 
+        }
+
+//CASE 2: Entry of Vehicle
+        if(digitalRead(IR1)==LOW && Slot!=0){
+          slotUpdate(Slot, flag1, flag2);  
+              gate.write(0);
+              flag1 = 1;
+              while(flag1){
+                lcdDisplay(" Please Standby!!"," ",90);
+                tone(buzzer,3000);
+                if( (digitalRead(IR2)==LOW) &&(digitalRead(IR1)==HIGH)){
+                   delay(500);
+                  break;
+                }
+               }
+              gate.write(100);
+              noTone(buzzer);
+               slotUpdate(Slot, flag1, flag2);
+               flag1 = 0;
+           
+        }
+
+//CASE 3: Exit of Vehicle
+        if(digitalRead(IR2)==LOW && Slot<5){
+          gate.write(0);
+          flag2 = 1;
+          while(flag2){
+            lcdDisplay(" Please Standby!!"," ",90);
+            tone(buzzer,3000);
+            if((digitalRead(IR2)==HIGH) && (digitalRead(IR1)==LOW)){
+              delay(500);
+              break;
+            }
+          }
+          gate.write(100);
+          noTone(buzzer);
+          slotUpdate(Slot, flag1, flag2);
+          flag2 = 0;
+        }
+  
+
+
  }  
